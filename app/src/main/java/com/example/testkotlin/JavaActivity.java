d3bbc6cbc2daa25d32c6449c2aedf3ded4a4b422
Gros19 <kjj6393@gmail.com>
2022-03-20 15:53:55 +0900

    intent로 MainActivity에서 JavaActivity로 로그인 ID값 넘기기

--- app/src/main/java/com/example/testkotlin/JavaActivity.java
+++ app/src/main/java/com/example/testkotlin/JavaActivity.java
@@ -3,6 +3,7 @@ package com.example.testkotlin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class JavaActivity extends AppCompatActivity {
 
@@ -10,7 +11,16 @@ public class JavaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_java);
+
+
+
+        TextView textView=findViewById(R.id.tv_result);
+        System.out.println("여기까지");
+        textView.setText(String.format("%s님 환영합니다.",getIntent().getExtras().getString("etId")));
+
     }
+
+
 
 
 }
--- app/src/main/java/com/example/testkotlin/MainActivity.kt
+++ app/src/main/java/com/example/testkotlin/MainActivity.kt
@@ -1,54 +1,129 @@
 package com.example.testkotlin
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.TextView
 import android.widget.Toast
-import androidx.core.view.isVisible
+import androidx.appcompat.app.AppCompatActivity
+import com.example.testkotlin.databinding.ActivityJavaBinding
 import com.example.testkotlin.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
-    val a : Int = 2
-    var b = 2 //가능
+//    val a : Int = 2
+//    var b = 2 //가능
 
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
 
-//        val vbinding = ActivityMainBinding.inflate(layoutInflater)
-//        setContentView(R.layout.activity_main)
-//        vbinding.visibleBT.setOnClickListener{
-//
+
+        /*마지막 EditText 완료버튼은 Login호출*/
+        binding.etPass.setOnEditorActionListener{v, actionId, event ->
+                if(actionId == EditorInfo.IME_ACTION_DONE){
+                    println(actionId)
+                    Login(v)
+                    true
+                }else false
+            }
+    }
+    fun Adenticate(): Boolean{
+        return binding.etEmail.text.toString() == "asdf@gmail.com" &&
+                binding.etAge.text.toString() == "32" &&
+                binding.etName.text.toString() == "Kim" &&
+                binding.etId.text.toString() == "kkk" &&
+                binding.etPass.text.toString() == "asdf"
+    }
+
+    fun Login(view: View) {
+        /*키보드 숨기기*/
+        var imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+
+        /*입력정보 비교*/
+        var  msg : String = ""
+        if(Adenticate()){
+            msg="로그인 성공"
+            println("로그인 성공")
+            /*엑티비티 이동*/
+            val nextIntent = Intent(this, JavaActivity::class.java)
+            nextIntent.putExtra("etId", binding.etId.text.toString())
+            startActivity(nextIntent)
+
+            /*엑티비티 이동 방법2*/
+            /*setContentView(R.layout.activity_java)
+            val textView: TextView =findViewById(R.id.tv_result)
+            textView.setText("${binding.etId.text}님 환영합니다.")*/
+
+
+
+        }else{
+            msg="로그인 실패"
+            println("로그인 실패")
+        }
+        println("${binding.etEmail.text} ${binding.etAge.text} ${binding.etName.text} ${binding.etPass.text} ${binding.etId.text}")
+
+        var t = Toast.makeText(this, msg,Toast.LENGTH_LONG)
+        t.setGravity(Gravity.TOP, 0, 0)
+        t.show()
+    }
+}
+
+
+//        val loginVinding = ActivityMainBinding.inflate(layoutInflater)
+//        loginVinding.loginBtn.setOnClickListener {
+//            println("일단 여기까지")
+//            loginVinding.etPass.setOnEditorActionListener{v, actionId, event ->
+//                if(actionId == EditorInfo.IME_ACTION_DONE){
+//                    login(v)
+//                    true
+//                }else false
+//            }
 //        }
 
 
 
-        fun DDuck(v : View){
-            println("뚝!!")
-        }
-        val binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        var cnt = 0;
-        binding.dd.setOnClickListener{
-            DDuck(it)
-            binding.dd.text = "${cnt++}"
-        }
 
 
 
-        binding.visibleBtOn.setOnClickListener {
-//            binding.visibleBT.isVisible = true
-            binding.visibleBT.visibility = View.VISIBLE
-            binding.emilLink.isEnabled = true
-        }
-        binding.visibleBT.setOnClickListener {
-//            binding.visibleBT.isVisible=false
-            binding.visibleBT.visibility = View.INVISIBLE
 
 
-        }
+
+
+//        fun DDuck(v : View){
+//            println("뚝!!")
+//        }
+//        val binding = ActivityMainBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+//        var cnt = 0;
+//        binding.dd.setOnClickListener{
+//            DDuck(it)
+//            binding.dd.text = "${cnt++}"
+//        }
+//
+//
+//
+//        binding.visibleBtOn.setOnClickListener {
+////            binding.visibleBT.isVisible = true
+//            binding.visibleBT.visibility = View.VISIBLE
+//            binding.emilLink.isEnabled = true
+//        }
+//        binding.visibleBT.setOnClickListener {
+////            binding.visibleBT.isVisible=false
+//            binding.visibleBT.visibility = View.INVISIBLE
+//
+//
+//        }
 //        //바인딩 초기화
 //        val binding = ActivityMainBinding.inflate(layoutInflater)
 //        //레이아웃(root뷰) 표시
@@ -61,71 +136,71 @@ class MainActivity : AppCompatActivity() {
 //        }
 
 
-        val items : List<String> = listOf("apple", "bannana", "kiwifruit")
-        for(index in items.indices){
-            println("for문: $index   ${items[index]}")
-        }
-
-        var index = 0
-        while(index < items.size){
-            println("while문: ${index} ${items[index++]}")
-        }
+//        val items : List<String> = listOf("apple", "bannana", "kiwifruit")
+//        for(index in items.indices){
+//            println("for문: $index   ${items[index]}")
+//        }
+//
+//        var index = 0
+//        while(index < items.size){
+//            println("while문: ${index} ${items[index++]}")
+//        }
 
 //        의도적으로 무한루프를 발생
 //        앱이 강제로 꺼지지는 않았다
 //        while(true)println("좋아")
 
-        fun describe(obj: Any): String =
-            when (obj){
-                1       ->  "One"
-                "Hello" ->  "Greeting"
-                is Long ->  "Long"
-                !is String->    "Not a string"
-                else    ->  "Unknown"
-            }
-
-        val x = 10
-        val y = 9
-        if( x >= 1 && x <= y+1){
-            println("범위 안")
-        }
-        if(x in 1..y+1){
-            println("범위 안")
-        }
-
-        val list : List<String> = listOf("a", "b", "c")
-
-
-        if (-1 !in 0..list.lastIndex){
-            println("-1은 범위 안에 없다.")
-        }
-        if (list.size !in list.indices){
-            println("리스트 인덱스를 넘어갔다.")
-            println("list.indices: ${list.indices}")
-        }
-
-        for( x in 1..5){
-            println(x)
-        }
-
-
-        for (x in 1..10 step 2) println(x)
-        println("개행")
-        for(x in 9 downTo 0 step 3)println(x)
-
-
-        val items2 : Set<String> = setOf("apple", " banana", "kiwifruit")
-        when{
-            "orange" in items2 -> println("juicy")
-            "apple" in items2 -> println("apple is fine too")
-        }
-
-        val fruits : List<String> = listOf("banana", "avocado", "apple", "kiwifruit")
-        fruits
-            .filter{it.startsWith("a")}
-            .sortedBy{it}
-            .map{it.toUpperCase() }
-            .forEach{println(it)}
+//        fun describe(obj: Any): String =
+//            when (obj){
+//                1       ->  "One"
+//                "Hello" ->  "Greeting"
+//                is Long ->  "Long"
+//                !is String->    "Not a string"
+//                else    ->  "Unknown"
+//            }
+//
+//        val x = 10
+//        val y = 9
+//        if( x >= 1 && x <= y+1){
+//            println("범위 안")
+//        }
+//        if(x in 1..y+1){
+//            println("범위 안")
+//        }
+//
+//        val list : List<String> = listOf("a", "b", "c")
+//
+//
+//        if (-1 !in 0..list.lastIndex){
+//            println("-1은 범위 안에 없다.")
+//        }
+//        if (list.size !in list.indices){
+//            println("리스트 인덱스를 넘어갔다.")
+//            println("list.indices: ${list.indices}")
+//        }
+//
+//        for( x in 1..5){
+//            println(x)
+//        }
+//
+//
+//        for (x in 1..10 step 2) println(x)
+//        println("개행")
+//        for(x in 9 downTo 0 step 3)println(x)
+//
+//
+//        val items2 : Set<String> = setOf("apple", " banana", "kiwifruit")
+//        when{
+//            "orange" in items2 -> println("juicy")
+//            "apple" in items2 -> println("apple is fine too")
+//        }
+//
+//        val fruits : List<String> = listOf("banana", "avocado", "apple", "kiwifruit")
+//        fruits
+//            .filter{it.startsWith("a")}
+//            .sortedBy{it}
+//            .map{it.toUpperCase() }
+//            .forEach{println(it)}
 
 //        println(describe(1))
 //        println(describe("Hello"))
@@ -151,41 +226,41 @@ class MainActivity : AppCompatActivity() {
 //        println(maxOfLift(3, 4))
 //        println(parseInt("100"))
 //        println(parseInt("호밀밭의"))
-    }
-    fun getStringLength(obj: Any): Int?{
-        if( obj is String) {
-            //여기서는 obj가 String
-            return obj.length
-        }
-        return null
-    }
 
-    fun parseInt(str: String): Int?{ // Int? 리턴 Int형인데 null 가능
-        var myNum : Int? = null
-        try{ myNum = str.toInt()}
-        catch(nfe : NumberFormatException){
-            return null
-        }
-        return myNum
-    }
-
-    fun maxOfLift(a: Int, b: Int): Int{
-        return if(a > b){
-            a
-        }else {
-            b
-        }
-    }
-
-    fun maxOf(a: Int, b: Int): Int{
-        if(a > b){
-            return a
-        }else {
-            return b
-        }
-    }
-
-    fun maxOfMinimum(a: Int, b: Int) = if(a > b) a else b
+//    fun getStringLength(obj: Any): Int?{
+//        if( obj is String) {
+//            //여기서는 obj가 String
+//            return obj.length
+//        }
+//        return null
+//    }
+//
+//    fun parseInt(str: String): Int?{ // Int? 리턴 Int형인데 null 가능
+//        var myNum : Int? = null
+//        try{ myNum = str.toInt()}
+//        catch(nfe : NumberFormatException){
+//            return null
+//        }
+//        return myNum
+//    }
+//
+//    fun maxOfLift(a: Int, b: Int): Int{
+//        return if(a > b){
+//            a
+//        }else {
+//            b
+//        }
+//    }
+//
+//    fun maxOf(a: Int, b: Int): Int{
+//        if(a > b){
+//            return a
+//        }else {
+//            return b
+//        }
+//    }
+//
+//    fun maxOfMinimum(a: Int, b: Int) = if(a > b) a else b
 
 
 //    fun sum(a: Int, b: Int){
@@ -199,4 +274,3 @@ class MainActivity : AppCompatActivity() {
 //    println(sum(1, 2 f))
 //    fun sum(a: Int, b: Float) = a + b
 
-}
