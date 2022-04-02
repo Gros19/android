f0412bc8b9a8589885d3c8c169ea8f5cdcbcc3ee
Gros19 <kjj6393@gmail.com>
2022-04-03 00:03:47 +0900

    webView google, naver button

--- wepview/src/main/java/com/example/slideapp/ButtonGNActivity.kt
+++ wepview/src/main/java/com/example/slideapp/ButtonGNActivity.kt
@@ -2,6 +2,7 @@ package com.example.slideapp
 
 import android.os.Bundle
 import android.view.View
+import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 import com.example.slideapp.databinding.ActivityButtonGnactivityBinding
 
@@ -11,10 +12,19 @@ class ButtonGNActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        binding.web
+        binding.webView.webViewClient = WebViewClient()
+
+        Load(binding.googleBt)
+
+
     }
-    fun Load(v: View){
-        when(v.id){}
-        R.id.googleBt ->
+
+    fun Load(view: View) {
+        when(view.id){
+            R.id.googleBt -> binding.webView.loadUrl("https://www.google.com")
+            R.id.naverBt -> binding.webView.loadUrl("https://m.naver.com")
+        }
+
     }
+
 }
